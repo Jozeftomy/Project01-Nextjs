fe8a4a7b33ff773845d16acff5c635e5245eee85
Jozeftomy <josephtomy02@gmail.com>
2025-04-21 17:24:52 +0530

    removed body component

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from './component/Header';
-import Body from './component/Body';
 import Footer from './component/Footer';
 import Map from './component/Map';
 
@@ -9,7 +8,6 @@ const Home = () => {
     <div className="w-full max-w-screen overflow-hidden">
       <Header />
       <Map />
-      <Body/>
       <Footer />
     </div>
   );
